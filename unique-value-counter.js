8c5c00cd64b25813b9342736133d49a812822394
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-02-06 21:55:53 +0530

    unique values refactored added

--- unique-value-counter.js
+++ unique-value-counter.js
@@ -18,4 +18,17 @@ const countUniqueValues = (array) => {
   return firstPointerIndex;
 };
 
-console.log(countUniqueValues([1, 1, 1, 1, 1, 2, 5, 5]));
+// console.log(countUniqueValues([1, 1, 1, 1, 1, 2, 5, 5]));
+
+const countUniqueValuesRefactored = (array) => {
+  let i = 0;
+  for (let j = 1; j < array.length; j++) {
+    if (array[i] !== array[j]) {
+      i++;
+      array[i] = array[j];
+    }
+  }
+  return i + 1;
+};
+
+console.log(countUniqueValuesRefactored([1, 1, 1, 1, 1, 2, 5, 5]));
