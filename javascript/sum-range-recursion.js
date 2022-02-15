0b4ea8a0b03efef44000fbae89a255c84ea421e7
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-02-15 10:07:28 +0530

    SUM RANGE ADDED

---
+++ javascript/sum-range-recursion.js
@@ -0,0 +1,6 @@
+var sumRange = function (number) {
+    if (number === 1)
+        return 1;
+    return number + sumRange(number - 1);
+};
+console.log(sumRange(4));
---
+++ typescript/sum-range-recursion.ts
@@ -0,0 +1,5 @@
+const sumRange = (number: number) => {
+  if (number === 1) return 1;
+  return number + sumRange(number - 1);
+};
+console.log(sumRange(4));
