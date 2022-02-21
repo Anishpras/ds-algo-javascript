b8824e68e05a41f15fcd707503d3ad074e207816
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-02-22 00:09:41 +0530

    binary not working

---
+++ typescript/binary-search.ts
@@ -0,0 +1,19 @@
+function binarySearch(arr, val) {
+  // add whatever parameters you deem necessary - good luck!
+  let left = 0;
+  let right = arr.length - 1;
+  let middle = (left + right) / 2;
+
+  while (left < right) {
+    if (arr[middle] === val) return middle;
+    if (arr[middle] < val) {
+      left = middle;
+      middle = (left + right) / 2;
+    }
+    right = middle;
+    middle = (left + right) / 2;
+  }
+  return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 5));
