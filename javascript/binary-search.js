db4f49b400cd93d69919b35f9abe77db53ac204d
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-02-22 22:35:14 +0530

    binary added

--- javascript/binary-search.js
+++ javascript/binary-search.js
@@ -2,17 +2,18 @@ function binarySearch(arr, val) {
     // add whatever parameters you deem necessary - good luck!
     var left = 0;
     var right = arr.length - 1;
-    var middle = (left + right) / 2;
-    while (left < right) {
-        if (arr[middle] === val)
-            return middle;
-        if (arr[middle] < val) {
-            left = middle;
-            middle = (left + right) / 2;
+    var middle = Math.floor((left + right) / 2);
+    while (arr[middle] !== val) {
+        if (val < arr[middle]) {
+            right = middle - 1;
+            middle = Math.floor((left + right) / 2);
         }
-        right = middle;
-        middle = (left + right) / 2;
+        left = middle + 1;
+        middle = Math.floor((left + right) / 2);
+    }
+    if (arr[middle] === val) {
+        return middle;
     }
     return -1;
 }
-console.log(binarySearch([1, 2, 3, 4, 5], 5));
+console.log(binarySearch([1, 2, 3, 4, 5], 8));
--- typescript/binary-search.ts
+++ typescript/binary-search.ts
@@ -1,19 +1,33 @@
-function binarySearch(arr, val) {
-  // add whatever parameters you deem necessary - good luck!
-  let left = 0;
-  let right = arr.length - 1;
-  let middle = (left + right) / 2;
-
-  while (left < right) {
-    if (arr[middle] === val) return middle;
-    if (arr[middle] < val) {
-      left = middle;
-      middle = (left + right) / 2;
+// Original Solution
+function binarySearch(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
     }
-    right = middle;
-    middle = (left + right) / 2;
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === elem) {
+    return middle;
   }
   return -1;
 }
 
-console.log(binarySearch([1, 2, 3, 4, 5], 5));
+// Refactored Version
+function binarySearchRefactored(arr, elem) {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
+}
+
+binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103);
