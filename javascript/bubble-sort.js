4073e0987b45cec7da624bb50cfc7bdc4a890987
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-02-27 23:43:26 +0530

    bubble sort done

---
+++ javascript/bubble-sort.js
@@ -0,0 +1,13 @@
+var BubbleSort = function (array) {
+    for (var i = array.length; i > 0; i--) {
+        for (var j = 0; j < i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                var temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+    return array;
+};
+console.log(BubbleSort([1, 4, 9, 8, 11, 10]));
---
+++ typescript/bubble-sort.ts
@@ -0,0 +1,14 @@
+const BubbleSort = (array) => {
+  for (let i = array.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+};
+
+console.log(BubbleSort([1, 4, 9, 8, 11, 10]));
