d5abf68bb22719655a0e725fdd9250138b09e1db
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-03-20 21:07:28 +0530

    unshift done

--- javascript/doubly-linked-list.js
+++ javascript/doubly-linked-list.js
@@ -56,4 +56,18 @@ class DoublyLinkedList {
     this.length--;
     return oldHead;
   }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
