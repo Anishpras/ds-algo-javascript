24214c7ea31275d25295fc89fd85938dccd9b9a3
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-03-24 23:18:35 +0530

    queue added

---
+++ javascript/queue.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = this.first;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+    var temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
