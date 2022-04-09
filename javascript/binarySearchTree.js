7380f0f0776df23339d956ed373806aca4f59326
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-04-10 02:25:13 +0530

    DFS PreOrder

--- javascript/binarySearchTree.js
+++ javascript/binarySearchTree.js
@@ -86,4 +86,17 @@ class BinarySearchTree {
 
     return data;
   }
+
+  DFSPreOrder() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  
 }
