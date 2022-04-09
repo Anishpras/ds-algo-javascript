be202209334bb78bf603c2eb71f23a01d821d6ff
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-04-10 02:27:01 +0530

    DFS PostOrder

--- javascript/binarySearchTree.js
+++ javascript/binarySearchTree.js
@@ -98,5 +98,14 @@ class BinarySearchTree {
     return data;
   }
 
-  
+  DFSPostOrder(){
+    let data = [];
+    function traverse(node){
+      if(node.left) traverse(node.left);
+      if(node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
