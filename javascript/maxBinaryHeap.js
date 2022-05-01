4a1b4aa68247cb0ec34f0e148049750a2c28720e
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-05-01 22:52:18 +0530

    extract Max done

--- javascript/maxBinaryHeap.js
+++ javascript/maxBinaryHeap.js
@@ -19,5 +19,43 @@ var MaxBinaryHeap = /** @class */ (function () {
             index = parentIndex;
         }
     };
+    MaxBinaryHeap.prototype.extractMax = function () {
+        var max = this.values[0];
+        var end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    };
+    MaxBinaryHeap.prototype.sinkDown = function () {
+        var index = 0;
+        var length = this.values.length;
+        var element = this.values[0];
+        while (true) {
+            var leftChildIndex = 2 * index + 1;
+            var rightChildIndex = 2 * index + 2;
+            var leftChild = void 0, rightChild = void 0;
+            var swap = null;
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                if (leftChild > element) {
+                    swap = leftChildIndex;
+                }
+            }
+            if (rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex];
+                if ((swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)) {
+                    swap = rightChildIndex;
+                }
+            }
+            if (swap === null)
+                break;
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
+    };
     return MaxBinaryHeap;
 }());
